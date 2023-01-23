0a8996b76904914d017a4113aba6c3ae9429e0a0
Hernán López <hernan.lopez@kdsoft.io>
2023-01-23 19:36:01 -0300

    Update project to current LTS .NET Core version given it's something from the ground up and packages. Added new Project Libraries to organize logic, domain, repositories, infrastructure

---
+++ Sat.Recruitment.Domain/Repositories/IUsersRepository.cs
@@ -0,0 +1,16 @@
+﻿using Sat.Recruitment.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.Domain.Repositories
+{
+    public interface IUsersRepository
+    {
+        void CreateUser(User newUser);
+        IEnumerable<User> GetUsers();
+
+    }
+}
---
+++ Sat.Recruitment.FileRepository/FileUsersRepository.cs
@@ -0,0 +1,26 @@
+﻿using Sat.Recruitment.Domain.Models;
+using Sat.Recruitment.Domain.Repositories;
+
+namespace Sat.Recruitment.FileRepository
+{
+    public class FileUsersRepository : IUsersRepository
+    {
+        public void CreateUser(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            var usersList = new List<User>();
+
+            var path = Directory.GetCurrentDirectory() + "/Files/Users.txt";
+
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+
+            StreamReader reader = new StreamReader(fileStream);
+
+            return usersList;
+        }
+    }
+}
